9a6c31760bff678fb50ae6eafc54edc61c0362f4
GkuZzz <117741080+GkuZzz@users.noreply.github.com>
2023-10-26 22:42:47 +0300

    feat: add adaptive

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const burgerButton = document.querySelector('.header__burger');
 const navigation = document.querySelector('.header__nav');
-
+const video = document.querySelector('.video__text')
 
 
 burgerButton.addEventListener('click', () => {
     navigation.classList.toggle('active')
     burgerButton.classList.toggle('active')
+
+    burgerButton.classList.contains('active') ? video.style.display = 'none' : video.style.display = 'block'
 })
