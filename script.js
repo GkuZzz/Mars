e4a9f45ad5f07db0ee89aeca887fd588ae8ed567
GkuZzz <117741080+GkuZzz@users.noreply.github.com>
2023-10-27 17:03:18 +0300

    feat: add loading and popup

--- script.js
+++ script.js
@@ -20,3 +20,42 @@ document.addEventListener('mousemove', (e) => {
     const yPos = (e.clientY / window.innerHeight) * 100;
     body.style.backgroundPosition = `${xPos}% ${yPos}%`;
 });
+
+
+// LOADING
+
+document.addEventListener('DOMContentLoaded', () => {
+    const deco = document.querySelector('.deco');
+    const welcome = document.querySelector('.welcome-text');
+    const title = document.querySelector('.video__title');
+    const video = document.querySelector('.video')
+    setTimeout(() => {
+        deco.style.opacity = '1';
+        welcome.style.opacity = '1'
+    }, 800)
+
+    setTimeout(() => {
+        title.style.opacity = '1';
+        // video.style.opacity = '1'
+    }, 1200)
+
+    setTimeout(() => {
+        video.style.opacity = '1'
+    }, 1000)
+})
+
+
+
+// POPUP
+
+const popupButton = document.querySelector('.header__button');
+const closePopupButton = document.querySelector('.exit__button')
+const popup = document.querySelector('.popup');
+
+popupButton.addEventListener('click', () => {
+    popup.style.display = 'block'
+})
+
+closePopupButton.addEventListener('click', () => {
+    popup.style.display = 'none'
+})
