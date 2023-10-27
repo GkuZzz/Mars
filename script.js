a537cce4fc3af5fe321ad13513b4c8b3e987f066
GkuZzz <117741080+GkuZzz@users.noreply.github.com>
2023-10-27 14:31:26 +0300

    feat: add parallax

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// BURGER
+
 const burgerButton = document.querySelector('.header__burger');
 const navigation = document.querySelector('.header__nav');
 const video = document.querySelector('.video__text')
@@ -9,3 +11,12 @@ burgerButton.addEventListener('click', () => {
 
     burgerButton.classList.contains('active') ? video.style.display = 'none' : video.style.display = 'block'
 })
+
+// PARALAX
+
+document.addEventListener('mousemove', (e) => {
+    const body = document.querySelector('body');
+    const xPos = (e.clientX / window.innerWidth) * 100;
+    const yPos = (e.clientY / window.innerHeight) * 100;
+    body.style.backgroundPosition = `${xPos}% ${yPos}%`;
+});
